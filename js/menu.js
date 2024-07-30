c7fd561029378570d133b798b0fd9cda22161d81
Yeung <@Yeung>
2024-07-30 21:05:20 +0900

    r6-7-30 mac 21:05

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 
 
-var nav_menuButton = document.getElementById('toggle-menu');
+const nav_menuButton = document.getElementById('toggle-menu');
 nav_menuButton.addEventListener('click', function(event) {
     event.preventDefault();
-    var menu = document.getElementById('main-menu');
+    const menu = document.getElementById('main-menu');
     menu.classList.toggle('is-open');
 });
